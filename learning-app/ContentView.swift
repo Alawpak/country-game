4db06da0834800472680eabb1f910fde59e222ca
Alawpak <alan281@hotmail.es>
2024-10-26 20:09:09 -0600

    Initial Commit

---
+++ learning-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learning-app
+//
+//  Created by Alan Perez on 26/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learning-app/learning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_appApp.swift
+//  learning-app
+//
+//  Created by Alan Perez on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct learning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
